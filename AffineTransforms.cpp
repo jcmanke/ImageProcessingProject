ee7ce3d89a6dfb82b21d797c561b6135648664ab
Joe Manke <joseph.manke@mines.sdsmt.edu>
2015-04-12 18:10:39 -0600

    Made a function for getting scale factors and creating the new image.

--- AffineTransforms.cpp
+++ AffineTransforms.cpp
@@ -1,5 +1,23 @@
 #include "DIPProject.h"
 
+bool getScaledImage(Image &originalImage, Image &newImage)
+{
+    double x_scale = 1.0;
+    double y_scale = 1.0;
+
+    Dialog input = Dialog("Scaling").Add(x_scale, "X Scale Factor", 0.0, 10.0).Add(y_scale, "Y Scale Factor", 0.0, 10.0);
+
+    if (input.Show())
+    {
+        newImage = Image(originalImage.Height() * y_scale, originalImage.Width() * x_scale);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 bool ImageTransformations::Menu_Transformation_RotationByBilinearIntensity(Image &image)
 {
     return true;
@@ -12,10 +30,44 @@ bool ImageTransformations::Menu_Transformation_RotationByNearestNeighbor(Image &
 
 bool ImageTransformations::Menu_Transformation_ScaleByBilinearIntensity(Image &image)
 {
-    return true;
+    Image newImage;
+    if (getScaledImage(image, newImage))
+    {
+        for (uint y = 0; y < image.Height(); y++)
+        {
+            for (uint x = 0; x < image.Width(); x++)
+            {
+                //DO THE STUFF
+            }
+        }
+
+        image = newImage;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 bool ImageTransformations::Menu_Transformation_ScaleByNearestNeighbor(Image &image)
 {
-    return true;
+    Image newImage;
+    if (getScaledImage(image, newImage))
+    {
+        for (uint y = 0; y < image.Height(); y++)
+        {
+            for (uint x = 0; x < image.Width(); x++)
+            {
+                //DO THE STUFF
+            }
+        }
+
+        image = newImage;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
