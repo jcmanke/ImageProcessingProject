4cec1b1e544e7bfe80138772211788feee7e5d13
Joe Manke <joseph.manke@mines.sdsmt.edu>
2015-04-12 18:53:56 -0600

    Nearest neighbor scaling implemented

--- AffineTransforms.cpp
+++ AffineTransforms.cpp
@@ -1,4 +1,7 @@
 #include "DIPProject.h"
+#include <cmath>
+
+using namespace std;
 
 bool getScaledImage(Image &originalImage, Image &newImage)
 {
@@ -52,14 +55,26 @@ bool ImageTransformations::Menu_Transformation_ScaleByBilinearIntensity(Image &i
 
 bool ImageTransformations::Menu_Transformation_ScaleByNearestNeighbor(Image &image)
 {
-    Image newImage;
-    if (getScaledImage(image, newImage))
+    double x_scale = 1.0;
+    double y_scale = 1.0;
+
+    Dialog input = Dialog("Scaling").Add(x_scale, "X Scale Factor", 0.0, 10.0).Add(y_scale, "Y Scale Factor", 0.0, 10.0);
+
+    if (input.Show())
     {
-        for (uint y = 0; y < image.Height(); y++)
+        Image newImage = Image(image.Height() * y_scale, image.Width() * x_scale);
+
+        for (uint y = 0; y < newImage.Height(); y++)
         {
-            for (uint x = 0; x < image.Width(); x++)
+            uint origY = y / y_scale + 0.5;
+            origY = min(origY, image.Height() - 1);
+
+            for (uint x = 0; x < newImage.Width(); x++)
             {
-                //DO THE STUFF
+                uint origX = x / x_scale + 0.5;
+                origX = min(origX, image.Width() - 1);
+
+                newImage[y][x] = image[origY][origX];
             }
         }
 
