ea50d6d7c6417ef299f53e6adb8d2439e2aae008
Jonesman64 <jonathan.tomes@mines.sdsmt.edu>
2015-04-27 17:20:21 -0600

    Obtaining the needed points
    I know the equations and I have the points.. I'm currently uncertain how to put the points into the equations... I may need to ask dr Weiss tomorrow if it can't be figured out.

--- RubberSheet.cpp
+++ RubberSheet.cpp
@@ -1,4 +1,5 @@
 #include "DIPProject.h"
+#include "point.h"
 
 bool ImageTransformations::Menu_Transformation_GeneralWarp(Image &image)
 {
@@ -23,7 +24,17 @@ bool ImageTransformations::Menu_Transformation_GeneralWarp(Image &image)
         // g = d2 - a2
         // h = a2
 
-
+        QPoint pointA, pointB, pointC,pointD, pointE, pointF, pointG, pointH;
+        pointA = stp->Point(0, 0).toPoint();
+        pointB = stp->Point(0, stp->Cols()-1).toPoint();
+        pointC = stp->Point(stp->Rows()-1, 0).toPoint();
+        pointD = stp->Point(stp->Rows()-1, stp->Cols()-1).toPoint();
+        pointE = ttp->Point(0,0).toPoint();
+        pointF = ttp->Point(0, ttp->Cols()-1).toPoint();
+        pointG = ttp->Point(ttp->Rows()-1, 0).toPoint();
+        pointH = ttp->Point(ttp->Rows()-1, ttp->Cols()-1).toPoint();
+        // I am confused on how to use the 8 points in the equations...
+        // might have to ask Dr. Weiss tomorrow if no one can figure it out...
 
         return true;
     }
